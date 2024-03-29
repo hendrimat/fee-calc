c63c3dd8c1061bdaec895483400b6a9582c275b8
hendrimat <hendrik.matvejev@gmail.com>
2024-03-29 13:43:44 +0200

    Initial commit

---
+++ src/main/java/com/fujitsu/trial/feecalc/FeeCalcApplication.java
@@ -0,0 +1,13 @@
+package com.fujitsu.trial.feecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeeCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fujitsu/trial/feecalc/FeeCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujitsu.trial.feecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
